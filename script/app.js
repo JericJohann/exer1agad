3968d347c93e90cee87eafa6b8b93b085a7ade5c
JericJohann <133608538+JericJohann@users.noreply.github.com>
2023-09-08 10:11:49 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -2,19 +2,19 @@ function displayProduct() {
   let firstNum = Number(document.getElementById('firstNum').innerHTML)
   let secondNum = Number(document.getElementById('secondNum').innerHTML)
 
-  let total = firstNum + secondNum;
+  let total = firstNum * secondNum;
   document.getElementById("answer").innerHTML = ` ${firstNum} * ${secondNum}, equals to ${total}` ;
 }
 
-document.getElementById('multiplyButton').addEventListener("click", displaySum);
+document.getElementById('multiplyButton').addEventListener("click", displayProduct);
 
 function displayDiff() {
   let firstNum = Number(document.getElementById('firstNum').innerHTML)
   let secondNum = Number(document.getElementById('secondNum').innerHTML)
 
-  let total = firstNum + secondNum;
-  document.getElementById("answer").innerHTML = ` ${firstNum} * ${secondNum}, equals to ${total}` ;
+  let total = firstNum - secondNum;
+  document.getElementById("answer").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ;
 }
 
-document.getElementById('multiplyButton').addEventListener("click", displaySum);
+document.getElementById('diffButton').addEventListener("click", displayDiff);
 
